c541dc3efdde575500c2ed17b5b5612e170dabf7
Matin-Ahmadi-2002 <miranahmadi2002@gmail.com>
2024-02-05 23:45:48 -0800

    header responsive complete

--- js/main.js
+++ js/main.js
@@ -8,3 +8,18 @@ window.addEventListener('scroll', function() {
       header.style.backgroundColor = 'transparent'; /* رنگ پیشفرض هدر */
     }
   });
+
+  let menu = document.querySelector(".menu_responsive")
+  let menuBtn = document.querySelector(".hamberger_menu")
+  let menuBtnIcon = document.querySelector(".hamberger_menu i")
+
+  menuBtn.addEventListener("click", function(){
+    if(menuBtnIcon.classList.contains("fa-bars")){
+      menu.style.right = "0"
+      menuBtnIcon.classList = "fa fa-times"
+    } else {
+      menu.style.right = "-200px"
+      menuBtnIcon.classList = "fa fa-bars"
+    }
+  })
+  
